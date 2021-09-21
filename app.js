9ef1d519f7210765d6d4b6f951c84814e20848b7
ronpe <ron.peretz1@gmail.com>
2021-09-21 23:09:30 +0300

    implement server with mongoDB

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const indexRouter = require('./routes/index');
+const app = express();
+const http = require('http');
+const db = require("./db");
+
+const port = 4999;
+
+app.set('port', port);
+
+app.use(cors());
+
+app.use('/', indexRouter);
+const server = http.createServer(app);
+
+
+db.initDb((err, db) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(`server listening to port: ${port}`);
+        server.listen(port, '0.0.0.0');
+    }
+});
+
+module.exports = app;
